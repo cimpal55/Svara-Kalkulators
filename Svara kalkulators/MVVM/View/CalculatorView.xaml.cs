3849744c1d8be20aad9f74ed992db7be3c72a8e8
cimpal55 <cimpals71@gmail.com>
2022-05-20 14:41:26 +0300

    added project

---
+++ Svara kalkulators/MVVM/View/CalculatorView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Svara_kalkulators.MVVM.View
+{
+    /// <summary>
+    /// Логика взаимодействия для HomeView.xaml
+    /// </summary>
+    public partial class CalculatorView : UserControl
+    {
+        public CalculatorView()
+        {
+            InitializeComponent();
+        }
+        private void ModesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender.Equals(PlusBtn))
+            {
+                Text_Mode.Foreground = Brushes.Green; ;
+                Text_Mode.Text = "Plus";
+            } else if (sender.Equals(MinusBtn)) {
+                Text_Mode.Foreground = Brushes.Red;
+                Text_Mode.Text = "Minus";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Svara kalkulators/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Svara_kalkulators.Core;
+
+namespace Svara_kalkulators.MVVM.ViewModel
+{
+    class MainViewModel : Observable
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand CalculatorViewCommand { get; set; }
+        public CalculatorViewModel CalcVm { get; set; }
+        public HomeViewModel HomeVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            CalcVm = new CalculatorViewModel();
+            HomeVm = new HomeViewModel();
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVm; });
+            CalculatorViewCommand = new RelayCommand(o => { CurrentView = CalcVm; });
+        }
+
+    }
+}
